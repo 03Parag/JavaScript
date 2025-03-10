ff8a11f3b1d4f9e19b18f5f312f28d4c9f02105f
Parag Agarwal <0111al211117.parag@gmail.com>
2025-03-10 13:42:25 +0530

    [Parag Agrawal] Added problem to write a program to find the digits that are repeated twice like 33, 77,  etc and store them in an array

---
+++ Day02_Array_Map/Array_Practice_Problems/Store_Repeated_Number.js
@@ -0,0 +1,14 @@
+function findRepeatedDigitNumbers() {
+    let repeatedNumbers = [];
+    
+    for (let i = 10; i <= 99; i++) {
+        let strNum = i.toString();
+        if (strNum[0] === strNum[1]) {
+            repeatedNumbers.push(i);
+        }
+    }
+
+    return repeatedNumbers;
+}
+
+console.log("Numbers with repeated digits:", findRepeatedDigitNumbers());
