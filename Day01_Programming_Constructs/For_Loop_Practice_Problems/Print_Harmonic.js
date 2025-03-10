d7d4842f3f0237137fbc085b74b252fda606dbfa
Parag Agarwal <0111al211117.parag@gmail.com>
2025-03-10 12:19:11 +0530

    [Parag Agrawal] Added problem to write a program that takes a command-line argument n and prints the nth harmonic number

---
+++ Day01_Programming_Constructs/For_Loop_Practice_Problems/Print_Harmonic.js
@@ -0,0 +1,10 @@
+function harmonicNumber(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += 1 / i;
+    }
+    console.log(`H(${n}) = ${sum.toFixed(6)}`);
+}
+
+let n = parseInt(process.argv[2]);
+harmonicNumber(n);
